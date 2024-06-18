8fa05e175a7c55240d9e274a861b823ce1fcf536
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-18 18:51:27 +0100

    Update streamlit_app.py
    fix indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -20,20 +19,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
-    , max_selections = 5
-    )
+    , max_selections = 5)
 
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '"""+ name_on_order + """')"""
-
+        values ('""" + ingredients_string + """', '"""+ name_on_order + """')"""
+    
     time_to_insert= st.button('Submit Order')
     
     if time_to_insert:
@@ -41,8 +37,5 @@ if ingredients_list:
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-# New section to display fruityvice nutrition info
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
